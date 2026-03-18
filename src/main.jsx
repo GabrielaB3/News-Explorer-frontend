a7e35cbc9a2ff3cf215e521f7b6f4074d610f3ff
GabrielaBC <gbc4.1997@gmail.com>
2026-03-18 18:01:36 -0400

    docs: setup deployment scripts

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 import "./vendor/normalize.css";
@@ -7,8 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
